f0f6a5d5af1a699b7fe1d96a2577dc83305f6759
7S-Coder <dylan.e33@hotmail.fr>
2023-04-01 07:30:28 +0200

    js to create the logic on the button called all

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ fetch(urlPosts)
   .catch((error) => {
     console.error(error);
   });
+
 //Mise en place des boutons
 const portfolio = document.querySelector("#portfolio");
 
@@ -65,3 +66,38 @@ fetch(urlCategories)
 const allButtonElement = document.createElement("button");
 allButtonElement.innerText = "Tous";
 btnDiv.appendChild(allButtonElement);
+
+//Rendre les boutons dynamique :
+// le bouton Tous doit afficher TOUT les posts
+allButtonElement.addEventListener("click", function () {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+  console.log("click");
+
+  fetch(urlPosts)
+    .then((response) => response.json())
+    .then((apiData) => {
+      data = apiData;
+      for (let i = 0; i < data.length; i++) {
+        const post = data[i];
+        // Récupération de l'élément du DOM qui accueillera les figures
+        const gallery = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un post
+        const postElement = document.createElement("figure");
+        // Création des balises
+        const imageElement = document.createElement("img");
+        imageElement.src = post.imageUrl;
+        const descElement = document.createElement("figcaption");
+        descElement.innerText = post.title;
+
+        // On rattache la balise figure a la section Fiches
+        gallery.appendChild(postElement);
+        // On rattache l’image à pieceElement (la balise article)
+        postElement.appendChild(imageElement);
+        postElement.appendChild(descElement);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
