13492f00fb76cbb27bc5d46c1af124cbd91dd04f
7S-Coder <dylan.e33@hotmail.fr>
2023-04-02 11:54:17 +0200

    the functions are more relevant

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-console.log("hello world");
-// url pour appeler tout les posts
+// Url pour appeler tout les posts
 const urlPosts = "http://localhost:5678/api/works";
 
+// Url pour appeler les catégories
 const urlCategories = "http://localhost:5678/api/categories";
 
-// création d'une div qui va contenir les bouttons
+// Afficher les posts avec la catégorie tous par défault
+getAllPostsByCategory("tous").then((data) => createPosts(data));
 
-// GET //
-//Fetch les données et les stocker dans une variable
-function allPosts() {
-  fetch(urlPosts)
-    .then((response) => response.json())
-    .then((apiData) => {
-      data = apiData;
-      for (let i = 0; i < data.length; i++) {
-        const post = data[i];
-        // Récupération de l'élément du DOM qui accueillera les figures
-        const gallery = document.querySelector(".gallery");
-        // Création d’une balise dédiée à un post
-        const postElement = document.createElement("figure");
-        // Création des balises
-        const imageElement = document.createElement("img");
-        imageElement.src = post.imageUrl;
-        const descElement = document.createElement("figcaption");
-        descElement.innerText = post.title;
+// LES FONCTIONS
 
-        // On rattache la balise figure a la section Fiches
-        gallery.appendChild(postElement);
-        // On rattache l’image à pieceElement (la balise article)
-        postElement.appendChild(imageElement);
-        postElement.appendChild(descElement);
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+function createPosts(data) {
+  for (let i = 0; i < data.length; i++) {
+    const posts = data[i];
+    // Récupération de l'élément du DOM qui accueillera les figures
+    const gallery = document.querySelector(".gallery");
+    // Création d’une balise dédiée à un post
+    const postElement = document.createElement("figure");
+    // Création des balises
+    const imageElement = document.createElement("img");
+    imageElement.src = posts.imageUrl;
+    const descElement = document.createElement("figcaption");
+    descElement.innerText = posts.title;
+
+    // On rattache la balise figure a la section Fiches
+    gallery.appendChild(postElement);
+    // On rattache l’image à pieceElement (la balise article)
+    postElement.appendChild(imageElement);
+    postElement.appendChild(descElement);
+  }
+}
+
+//FILTRER les posts par leur catégories
+async function getAllPostsByCategory(category) {
+  try {
+    const response = await fetch(urlPosts);
+    const apiData = await response.json();
+    const filteredPosts = apiData.filter((post) => post.category === category);
+    const data = apiData;
+    console.log(filteredPosts);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 //Mise en place des boutons
@@ -70,35 +77,20 @@ btnDiv.appendChild(allButtonElement);
 
 //Rendre les boutons dynamique :
 // le bouton Tous doit afficher TOUT les posts
-allButtonElement.addEventListener("click", function () {
+/* allButtonElement.addEventListener("click", function () {
   const gallery = document.querySelector(".gallery");
   gallery.innerHTML = "";
-  console.log("click");
+  allPostsPlaced();
+  allButtonElement.classList.add("active");
+}); */
 
-  fetch(urlPosts)
-    .then((response) => response.json())
-    .then((apiData) => {
-      data = apiData;
-      for (let i = 0; i < data.length; i++) {
-        const post = data[i];
-        // Récupération de l'élément du DOM qui accueillera les figures
-        const gallery = document.querySelector(".gallery");
-        // Création d’une balise dédiée à un post
-        const postElement = document.createElement("figure");
-        // Création des balises
-        const imageElement = document.createElement("img");
-        imageElement.src = post.imageUrl;
-        const descElement = document.createElement("figcaption");
-        descElement.innerText = post.title;
-
-        // On rattache la balise figure a la section Fiches
-        gallery.appendChild(postElement);
-        // On rattache l’image à pieceElement (la balise article)
-        postElement.appendChild(imageElement);
-        postElement.appendChild(descElement);
-      }
-    })
-    .catch((error) => {
-      console.error(error);
+//En cliquant sur chaque bouton, je lui donne la classe active ou je l'enlève.
+const buttons = document.querySelectorAll(".btnDiv button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
     });
+    this.classList.add("active");
+  });
 });
